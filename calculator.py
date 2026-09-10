9977fae17e672b4e9f94fe0a131f8478d6c8bc04
mtabassum161-bit <mtabassum161@gmail.com>
2026-09-10 19:14:10 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
-
+#introduction 
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 print(f"Hello {name}!")
 print(f"You are {age} years old.")
 
-
+#simple calculator 
 
 num1 = float(input("Enter first number: "))
-operator = input("Enter operator (+, -, *, /): ")
 num2 = float(input("Enter second number: "))
+operator = input("Enter operator(+,-,*,/): ")
 
 if operator == "+":
     print(num1 + num2)
@@ -19,31 +19,22 @@ elif operator == "-":
 
 elif operator == "**":
     print(num1 ** num2)
+
+
+elif operator == "*":
+    print(num1 * num2)
+
+#if divide by 0
+
 elif operator == "/":
     if num2 == 0:
         print("Cannot divide by zero")
     else:
         print(num1 / num2)
 
-elif operator == "*":
-    print(num1 * num2)
+# Now scientific calculator 
 
-elif operator == "/":
-    print(num1 / num2)
+elif operator == "%":
+    print(num1 % num2)
 
-else:
-    print("Invalid operator")
 
-roll_numbers = [101, 105, 102, 101, 108, 105, 110]
-
-unique_roll_numbers = set(roll_numbers)
-
-print("Unique roll numbers:", unique_roll_numbers)
-
-def odd_even(number):
-    if number % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
-
-odd_even(7)
